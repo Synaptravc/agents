76dce7bcb2e581f200a1239bd13bb6226a45c830
Synaptra <suleman.joel@yahoo.com>
2025-01-18 00:58:31 +0100

    Add files via upload

---
+++ examples/creative_writing/run_train.py
@@ -0,0 +1,20 @@
+import os
+
+from agents import CreativeWritingDataset
+import litellm
+from agents.optimization.trainer import Trainer, TrainerConfig
+
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_BASE_URL"] = ""
+
+litellm.set_verbose = False
+
+if __name__ == "__main__":
+    # 准备数据
+    dataset = CreativeWritingDataset(split="train")
+
+    # Trainer训练
+    trainer_config_path = "/configs/trainer_config.json"
+    trainer = Trainer(config=TrainerConfig(
+        trainer_config_path), dataset=dataset)
+    trainer.train()
